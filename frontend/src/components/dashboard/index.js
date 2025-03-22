be2cc92d31d9385e99442e0406d284a8399e974a
Ram Veeramony <ram.veeramony@atinargroup.com>
2025-03-22 20:35:47 +1100

    Add dashboard components index file

---
+++ frontend/src/components/dashboard/index.js
@@ -0,0 +1,11 @@
+import DomainSummaryCard from './DomainSummaryCard';
+import RecentActivity from './RecentActivity';
+import ElementCountChart from './ElementCountChart';
+import DomainRelationshipMatrix from './DomainRelationshipMatrix';
+
+export {
+  DomainSummaryCard,
+  RecentActivity,
+  ElementCountChart,
+  DomainRelationshipMatrix
+};
