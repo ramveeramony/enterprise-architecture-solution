bb6509c51298df1eab628009639c07a5d8bb155d
Ram Veeramony <ram.veeramony@atinargroup.com>
2025-03-22 22:36:37 +1100

    Add routers package initialization file

---
+++ backend/routers/__init__.py
@@ -0,0 +1,11 @@
+"""
+Enterprise Architecture Solution - API Routers
+
+This package contains all the API routers for the Enterprise Architecture Solution.
+"""
+
+# Import routers so they can be imported from this package
+from . import models
+from . import elements
+from . import integrations
+from . import genai
